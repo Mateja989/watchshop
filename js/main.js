dacc656d77527754fc4d240367330b13495b8633
Mateja989 <matejamastelica309@gmail.com>
2022-04-12 12:06:11 +0200

    pr

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
 //da li je forma intuitivna
 //new i sale da li je okej
 //kod za dokumentaciju posto je templejt
-
+//za dohvatanje iz forme
+//za regularne izraze za adresu
 
 
 function dohvati(url,callback){
@@ -35,24 +36,25 @@ window.onload=function(){
     var kategorije=vratiLS('nizKategorija')
     var kolekcije=vratiLS('nizKolekcija')
 
-    console.log(url)
+    console.log(brendovi,kategorije,kolekcije)
+    
     brojProizvodaUKorpi()
 
-    if(url == "/watchshop/index.html" || url == "/watchshop/"){
+    if(url == "/index.html" || url == "/"){
         dohvati("satovi.json",najnovijiProizvodi)
         dohvati("satovi.json",rasprodaja)
         dohvati("kategorije.json",ispisKat)
         dohvati("brendovi.json",ispisBrendova)
         slider()
     }
-    if(url == "/watchshop/shop.html"){
+    if(url == "/shop.html"){
         dohvati("satovi.json",sviProizvodi)
         kreirajChb(brendovi,"#brend",'brendovi',".brendovi")
         kreirajChb(kategorije,"#kategorije",'kategorije',"#kategorije")
         kreirajChb(kolekcije,'#kolekcija','kolekcije','.kolekcije')
     }
-    if(url == "/watchshop/checkout.html"){
-        proveraForme()
+    if(url == "/checkout.html"){
+        validacijaPlacanja()
         ispisProizvodaZaKorpu()
         obrisiKorpu()
     }
@@ -338,156 +340,6 @@ function dostupnostArtikla(nizProizvoda){
     if(selektovani=="svi") return nizProizvoda
 }
 
-//glavna funkcija za proveru forme i placanje
-function proveraForme(){
-
-    //dohvatanje elemenata
-    var korisnkickoIme=document.querySelector('#korisnik')
-    var lozinka=document.querySelector('#lozinka')
-    var btnNastavi=document.querySelector('#btnNastavi')
-    var lozinkaProvera=document.querySelector('#btnProvera')
-    var ime=document.querySelector('#ime')
-    var prezime=document.querySelector('#prezime')
-    var mejl=document.querySelector('#email')
-    var ddlGrad=document.querySelector('#ddlGrad')
-    var posta=document.querySelector('#posta')
-    var ulica=document.querySelector('#ulica')
-    var telefon=document.querySelector('#telefon')
-    var pouzece=document.querySelector('#pouzece')
-    var kartica=document.querySelector('#kartica')
-    var karticaBroj=document.querySelector('#karticaBroj')
-    var btnZavrsiKupovinu=document.querySelector('#btnZavrsi')
-    var gradovi=["Beograd","Nis","Kragujevac","Kraljevo"]
-    var postanskiBrojevi=['11000','18000','34000','36000']
-
-    //regularni izrazi
-    var regIme=/^[A-Z]{1}[a-z]{2,14}$/
-    var regPrezime=/^[A-Z]{1}[a-z]{4,29}$/
-    var regLozinka=/^[A-Z]{1}[a-z0-9!@#$%^.&*]{7,19}$/
-    var regMejl=/^[a-zA-Z0-9]([a-z]|[0-9])+\.?-?_?([a-z]|[0-9])*\.?([a-z]|[0-9])*\@[a-z]{3,}\.([az]{2,4}\.)?([a-z]{2,4})$/
-    var regKorisnickoIme=/^([a-z]{1})[a-z0-9]{4,29}$/
-    var regKartica=/^[\d]{18}/
-    var regTelefon=/^06[\d]{7,8}/
-    var greska
-
-    //provera prve forme
-    logovanjeZaPlacanjeProvera()
-    function logovanjeZaPlacanjeProvera(){
-        btnNastavi.addEventListener('click',function(){
-
-            proveraPolja(korisnkickoIme,regKorisnickoIme)
-            proveraPolja(lozinka,regLozinka)
-
-            if(!greska){
-            otvoriZaPlacanje()
-            }
-        })
-    }
-    //provera kartice ako se izabere placanje karticom
-    kartica.addEventListener('blur',function(){
-        proveraPolja(kartica,regKartica)
-        if(!greska){
-            btnZavrsiKupovinu.removeAttribute('disabled')
-        }
-        else{
-            btnZavrsiKupovinu.setAttribute('disabled')
-        }
-    })
-    //provera forme posle klika i zavrsetak kupovine
-    btnZavrsiKupovinu.addEventListener('click',function(){
-        proveraPolja(ime,regIme)
-        proveraPolja(prezime,regPrezime)
-        proveraPolja(mejl,regMejl)
-        proveraPolja(telefon,regTelefon)
-        proveraPostanskiBroj()
-        if(!greska){
-            uspeh('#korisnikForma')
-            uspeh('#zaPlacanje')
-        } 
-    })
-
-    //funckija za brisanje forme posle uspesne kupovine
-    function uspeh(id){
-        var tagoviForme=document.querySelector(id)
-
-            for(let i=0;i<tagoviForme.length;i++){
-                tagoviForme[i].value=""
-            }
-
-        document.querySelector('.uspeh').classList.remove('nevidljiv')
-    }
-    //funkcija za prikazivanje lozinke u tekstualnom obliku lozinke
-    function proveriLozinku(){
-    
-        lozinka.setAttribute("type", "text"); 
-        function vratiLozinkuKriptovanu(){
-            lozinka.setAttribute("type", "password");
-        }
-            setTimeout(vratiLozinkuKriptovanu,500)
-        }
-    lozinkaProvera.addEventListener('click',proveriLozinku)
-
-    //aktiviranje druge forme
-    function otvoriZaPlacanje(){
-        var tagoviForme=document.querySelector('#zaPlacanje')
-        btnZavrsiKupovinu.removeAttribute('disabled')
-        for(let i=0;i<tagoviForme.length;i++){
-            tagoviForme[i].removeAttribute('disabled')
-            tagoviForme[i].style.background="#e6e6e6"
-        }
-    }
-
-    //provera tekstualnih polja
-    function proveraPolja(id,reg){
-        if(reg.test(id.value)){
-            id.nextElementSibling.classList.add('nevidljiv')
-            greska=false
-        }
-        else if(id.value==""){
-            id.nextElementSibling.classList.remove('nevidljiv')
-            greska=true
-        }
-        else{
-            id.nextElementSibling.classList.remove('nevidljiv')
-            greska=true
-        }
-    }
-    //ispisivanje gradova i provera grada i postanskog broja koji se automatski popunjava
-    gradPostanskiBrojIspis()
-    function gradPostanskiBrojIspis(){
-        let ispis="<option value='Izaberite'>Grad*</option>"
-        for(let grad of gradovi){
-            ispis+=`<option value=${grad}>${grad}</option>`
-        }
-        ddlGrad.innerHTML=ispis
-    }
-    function proveraPostanskiBroj(){
-        if(ddlGrad.value!="Izaberite"){
-            ddlGrad.nextElementSibling.classList.add('nevidljiv')
-            greska=false
-        }
-        else{
-            ddlGrad.nextElementSibling.classList.remove('nevidljiv')
-            greska=true
-        }
-    }
-    gradoviPostanskiBroj()
-    function gradoviPostanskiBroj(){
-        ddlGrad.addEventListener('change',function(){
-            if(ddlGrad.value!="Izaberite"){
-                let index=gradovi.findIndex(grad => grad==ddlGrad.value)
-                document.querySelector('#posta').value=postanskiBrojevi[index]
-                ddlGrad.nextElementSibling.classList.add('nevidljiv')
-                greska=false
-            }
-            else{
-                ddlGrad.nextElementSibling.classList.remove('nevidljiv')
-                greska=true
-            }
-        })
-    }
-}
-
 //modal
 let id = 0;
 function ispisiModal(nizProizvoda){
@@ -707,5 +559,170 @@ function obrisiKorpu(){
         localStorage.removeItem('proizvodiKorpa')
         ispisProizvodaZaKorpu()
     })
+}
+
+//dohvatanje elemenata
+var korisnkickoIme=document.querySelector('#korisnik')
+var lozinka=document.querySelector('#lozinka')
+var btnNastavi=document.querySelector('#btnNastavi')
+var lozinkaProvera=document.querySelector('#btnProvera')
+var ime=document.querySelector('#ime')
+var prezime=document.querySelector('#prezime')
+var mejl=document.querySelector('#email')
+var ddlGrad=document.querySelector('#ddlGrad')
+var posta=document.querySelector('#posta')
+var ulica=document.querySelector('#ulica')
+var telefon=document.querySelector('#telefon')
+var pouzece=document.querySelector('#pouzece')
+var kartica=document.querySelector('#kartica')
+var karticaBroj=document.querySelector('#karticaBroj')
+var btnZavrsiKupovinu=document.querySelector('#btnZavrsi')
+var gradovi=["Beograd","Nis","Kragujevac","Kraljevo"]
+var postanskiBrojevi=['11000','18000','34000','36000']
+
+//regularni izrazi
+var regIme=/^[A-Z]{1}[a-z]{2,14}$/
+var regPrezime=/^[A-Z]{1}[a-z]{4,29}$/
+var regLozinka=/^[A-Z]{1}[a-z0-9!@#$%^.&*]{7,19}$/
+var regMejl=/^[a-zA-Z0-9]([a-z]|[0-9])+\.?-?_?([a-z]|[0-9])*\.?([a-z]|[0-9])*\@[a-z]{3,}\.([az]{2,4}\.)?([a-z]{2,4})$/
+var regKorisnickoIme=/^([a-z]{1})[a-z0-9]{4,29}$/
+var regKartica=/^[\d]{18}/
+var regTelefon=/^06[\d]{7,8}/
+var greska
+
+//funkcija za proveru forme za placanje
+function validacijaPlacanja(){
+
+    //provera prve forme
+    logovanjeZaPlacanjeProvera()
+    function logovanjeZaPlacanjeProvera(){
+        btnNastavi.addEventListener('click',function(){
+
+            proveraPolja(korisnkickoIme,regKorisnickoIme)
+            proveraPolja(lozinka,regLozinka)
+
+            if(!greska){
+            otvoriZaPlacanje()
+            }
+        })
+    }
+    //provera kartice ako se izabere placanje karticom
+    kartica.addEventListener('blur',function(){
+        proveraPolja(kartica,regKartica)
+        if(!greska){
+            btnZavrsiKupovinu.removeAttribute('disabled')
+        }
+        else{
+            btnZavrsiKupovinu.setAttribute('disabled')
+        }
+    })
+    //provera forme posle klika i zavrsetak kupovine
+    btnZavrsiKupovinu.addEventListener('click',function(){
+        proveraPolja(ime,regIme)
+        proveraPolja(prezime,regPrezime)
+        proveraPolja(mejl,regMejl)
+        proveraPolja(telefon,regTelefon)
+        proveraPostanskiBroj()
+        if(!greska){
+            uspeh('#korisnikForma')
+            uspeh('#zaPlacanje')
+        } 
+    })
+
+    //funckija za brisanje forme posle uspesne kupovine
+    function uspeh(id){
+        var tagoviForme=document.querySelector(id)
+
+            for(let i=0;i<tagoviForme.length;i++){
+                tagoviForme[i].value=""
+            }
+
+        document.querySelector('.uspeh').classList.remove('nevidljiv')
+    }
+    //funkcija za prikazivanje lozinke u tekstualnom obliku lozinke
+    function proveriLozinku(){
+    
+        lozinka.setAttribute("type", "text"); 
+        function vratiLozinkuKriptovanu(){
+            lozinka.setAttribute("type", "password");
+        }
+            setTimeout(vratiLozinkuKriptovanu,500)
+        }
+    lozinkaProvera.addEventListener('click',proveriLozinku)
+
+    //aktiviranje druge forme
+    function otvoriZaPlacanje(){
+        var tagoviForme=document.querySelector('#zaPlacanje')
+        btnZavrsiKupovinu.removeAttribute('disabled')
+        for(let i=0;i<tagoviForme.length;i++){
+            tagoviForme[i].removeAttribute('disabled')
+            tagoviForme[i].style.background="#e6e6e6"
+        }
+    }
+
+    dogadjajPolje(korisnkickoIme,'blur',regKorisnickoIme)
+    dogadjajPolje(lozinka,'blur',regLozinka)
+    dogadjajPolje(ime,'blur',regIme)
+    dogadjajPolje(prezime,'blur',regPrezime)
+    dogadjajPolje(mejl,'blur',regMejl)
+    dogadjajPolje(telefon,'blur',regTelefon)
+
+
+    //funkcija za dodavanje dogadajaja polju
+    function dogadjajPolje(polje,dogadjaj,regEx){
+        polje.addEventListener(dogadjaj,function(){
+            proveraPolja(polje,regEx)
+        })
+    }
+
+    //provera tekstualnih polja
+    function proveraPolja(id,reg){
+        if(reg.test(id.value)){
+            id.nextElementSibling.classList.add('nevidljiv')
+            greska=false
+        }
+        else if(id.value==""){
+            id.nextElementSibling.classList.remove('nevidljiv')
+            greska=true
+        }
+        else{
+            id.nextElementSibling.classList.remove('nevidljiv')
+            greska=true
+        }
+    }
+    //ispisivanje gradova i provera grada i postanskog broja koji se automatski popunjava
+    gradPostanskiBrojIspis()
+    function gradPostanskiBrojIspis(){
+        let ispis="<option value='Izaberite'>Grad*</option>"
+        for(let grad of gradovi){
+            ispis+=`<option value=${grad}>${grad}</option>`
+        }
+        ddlGrad.innerHTML=ispis
+    }
+    function proveraPostanskiBroj(){
+        if(ddlGrad.value!="Izaberite"){
+            ddlGrad.nextElementSibling.classList.add('nevidljiv')
+            greska=false
+        }
+        else{
+            ddlGrad.nextElementSibling.classList.remove('nevidljiv')
+            greska=true
+        }
+    }
+    gradoviPostanskiBroj()
+    function gradoviPostanskiBroj(){
+        ddlGrad.addEventListener('change',function(){
+            if(ddlGrad.value!="Izaberite"){
+                let index=gradovi.findIndex(grad => grad==ddlGrad.value)
+                document.querySelector('#posta').value=postanskiBrojevi[index]
+                ddlGrad.nextElementSibling.classList.add('nevidljiv')
+                greska=false
+            }
+            else{
+                ddlGrad.nextElementSibling.classList.remove('nevidljiv')
+                greska=true
+            }
+        })
+    }
 }
 
