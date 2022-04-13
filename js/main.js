7e01485d08b647569e17f48631e5f2b5d0015ead
Mateja989 <matejamastelica309@gmail.com>
2022-04-13 02:09:46 +0200

    2414

--- js/main.js
+++ js/main.js
@@ -31,21 +31,21 @@ window.onload=function(){
 
     brojProizvodaUKorpi()
 
-    if(url == "/index.html" || url == "/"){
+    if(url == "/watchshop/index.html" || url == "/watchshop/"){
         dohvati("satovi.json",najnovijiProizvodi)
         dohvati("satovi.json",rasprodaja)
         dohvati("kategorije.json",ispisKat)
         dohvati("brendovi.json",ispisBrendova)
         slider()
     }
-    if(url == "/shop.html"){
+    if(url == "/watchshop/shop.html"){
         dohvati("satovi.json",sviProizvodi)
         kreirajChb(brendovi,"#brend",'brendovi',".brendovi")
         kreirajChb(kategorije,"#kategorije",'kategorije',"#kategorije")
         kreirajChb(kolekcije,'#kolekcija','kolekcije','.kolekcije')
         
     }
-    if(url == "/checkout.html"){
+    if(url == "/watchshop/checkout.html"){
         validacijaPlacanja()
         ispisProizvodaZaKorpu()
         obrisiKorpu()
